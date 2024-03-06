1b3f7c4e933484a8eb84ab6ddc9678974a447b94
TuanKiet318 <huynhtuankiet31804@gmail.com>
2024-03-06 10:45:50 +0700

    Hello

---
+++ MaterialDesignInXamlToolkit-master/MaterialDesignThemes.Wpf/Converters/BrushOpacityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MaterialDesignThemes.Wpf.Converters;
+
+public class BrushOpacityConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is SolidColorBrush brush)
+        {
+            var opacity = System.Convert.ToDouble(parameter, CultureInfo.InvariantCulture);
+            SolidColorBrush rv = new(brush.Color)
+            {
+                Opacity = opacity
+            };
+            rv.Freeze();
+            return rv;
+        }
+        return null;
+    }
+
+    public object? ConvertBack(object value, Type targetType, object? parameter, CultureInfo culture)
+        => Binding.DoNothing;
+}
---
+++ MaterialDesignInXamlToolkit-master/MaterialDesignThemes.Wpf/MenuAssist.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Media;
+
+namespace MaterialDesignThemes.Wpf;
+
+public static class MenuAssist
+{
+    #region AttachedProperty : TopLevelMenuItemHeight
+    public static readonly DependencyProperty TopLevelMenuItemHeightProperty
+        = DependencyProperty.RegisterAttached(
+            "TopLevelMenuItemHeight",
+            typeof(double),
+            typeof(MenuAssist));
+
+    public static double GetTopLevelMenuItemHeight(DependencyObject element) => (double)element.GetValue(TopLevelMenuItemHeightProperty);
+    public static void SetTopLevelMenuItemHeight(DependencyObject element, double value) => element.SetValue(TopLevelMenuItemHeightProperty, value);
+    #endregion
+}
